ab26c18d4b924cee2601d026ff2e726547525d42
hxx <845059331@qq.com>
2014-01-09 22:45:40 +0800

    Finish Microposts Display

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -53,10 +53,19 @@ describe "User pages" do
 
   describe "profile page" do
   	let(:user) { FactoryGirl.create(:user) }
-  	before { visit user_path(user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
 
-  	it {should have_content(user.name) }
-  	it {should have_title(user.name) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 
   describe "signup page" do
@@ -144,15 +153,6 @@ describe "User pages" do
       it { should have_link('Sign out', href: signout_path) }
       specify { expect(user.reload.name).to eq new_name }
       specify { expect(user.reload.email).to eq new_email }
-    end
-
-    describe "forbidden attributes" do
-      let(:params) do
-        { user: { admin: true, password: user.password,
-                  password_confirmation: user.password } }
-      end
-      before { patch user_path(user), params }
-      specify { expect(user.reload).not_to be_admin }
     end
 
     describe "forbidden attributes" do
